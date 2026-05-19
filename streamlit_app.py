7a3edaee675da95bc23518aa4a99a15de858d910
SobreCode <merwan.inge@gmail.com>
2026-05-19 09:10:16 +0200

    streamlit_app upload

---
+++ streamlit_app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+st.header('st.write')
+st.write("Hello *World* :sunglasses:")
+st.write(1234*4568)
+
+df = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=["a", "b", "c"]
+)
+
+c = alt.Chart(df).mark_circle().encode(
+    x="a", y="b", size="c", color="c"
+).properties(title='Hello')
+st.write(c)
